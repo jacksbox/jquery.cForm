f125c2ad0ef0e26045abd70ff32b11200661628d
jck7e <mario.j@jacksbox.de>
2015-07-07 21:48:58 +0200

    ADD plugin sceleton

--- js/plugins/jquery.cform.js
+++ js/plugins/jquery.cform.js
@@ -1,4 +1,39 @@
-(function($) {
-    $.cForm = function() {
-  };
+(function($){
+    $.cForm = function(element, options){
+        // To avoid scope issues, use 'base' instead of 'this'
+        // to reference this class from internal events and functions.
+        var magicHat = this;
+        
+        // Access to jQuery and DOM versions of element
+        magicHat.$element = $(element);
+        magicHat.element = element;
+        
+        // Add a reverse reference to the DOM object
+        magicHat.$element.data("cForm", magicHat);
+        
+        magicHat.init = function(){
+            magicHat.options = $.extend({},$.cForm.defaultOptions, options);
+            
+            // Put your initialization code here
+            magicHat.test();
+        };
+        
+        // Sample Function, Uncomment to use
+        magicHat.test = function(paramaters){
+        	console.log('test');
+        };
+        
+        // Run initializer
+        magicHat.init();
+    };
+    
+    $.cForm.defaultOptions = {
+    };
+    
+    $.fn.cForm = function(options){
+        return this.each(function(){
+            (new $.cForm(this, options));
+        });
+    };
+    
 })(jQuery);

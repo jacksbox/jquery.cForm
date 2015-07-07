7f196d0e9fa017b4b1b2cc345da5ffb629f237b8
jck7e <mario.j@jacksbox.de>
2015-07-07 22:47:05 +0200

    ADD first version element handler

--- js/plugins/jquery.cform.js
+++ js/plugins/jquery.cform.js
@@ -2,29 +2,71 @@
     $.cForm = function(element, options){
         // To avoid scope issues, use 'base' instead of 'this'
         // to reference this class from internal events and functions.
-        var magicHat = this;
+        var base = this;
         
         // Access to jQuery and DOM versions of element
-        magicHat.$element = $(element);
-        magicHat.element = element;
+        base.$element = $(element);
+        base.element = element;
         
         // Add a reverse reference to the DOM object
-        magicHat.$element.data("cForm", magicHat);
+        base.$element.data("cForm", base);
         
-        magicHat.init = function(){
-            magicHat.options = $.extend({},$.cForm.defaultOptions, options);
-            
+        base.init = function(){
+            base.options = $.extend({},$.cForm.defaultOptions, options);
+
+            var filter = ['input', 'textarea', 'select', 'button'];
+
             // Put your initialization code here
-            magicHat.test();
+            // INIT
+            if(base.$element.is(filter.join())){
+            	var tag = base.tagName(base.$element);
+
+            	if(tag === 'input') base.convertInput(base.$element);
+            }
         };
         
-        // Sample Function, Uncomment to use
-        magicHat.test = function(paramaters){
-        	console.log('test');
+        // converts inout fields
+        base.convertInput = function(node){
+        	var type = node.attr('type'),
+        		name = node.attr('name'),
+        		value = node.attr('value'),
+        		template = '',
+        		$html = $();
+
+            if (typeof type === typeof undefined && type === false) {
+				console.log('Error: Missing type-Attribute on input!');
+				return false;
+			}
+			if (typeof name === typeof undefined && name === false) {
+				name = '';
+			}
+			if (typeof value === typeof undefined && value === false) {
+				value = '';
+			}
+
+
+			switch(type) {
+			    case 'password':
+			    case 'text':
+			        break;
+			    case 'checkbox':
+			        break;
+			    case 'radio':
+			        break;
+			    case 'submit':
+			        break;
+			    default:
+					console.log('Error: No matching type was found - You will be forever alone!');
+			}
+			
         };
+
+        base.tagName = function($element) {
+  			return $element.prop("tagName").toLowerCase();
+		};
         
         // Run initializer
-        magicHat.init();
+        base.init();
     };
     
     $.cForm.defaultOptions = {

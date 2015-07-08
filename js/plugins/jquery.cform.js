ad8b544f0a5ee857626d088434b0ca920fd9c4a3
jck7e <mario.j@jacksbox.de>
2015-07-08 14:02:03 +0200

    ADD multiselect functionality && styles for multi- select

--- js/plugins/jquery.cform.js
+++ js/plugins/jquery.cform.js
@@ -116,38 +116,44 @@
 					console.log('Error: No matching was found - You will be forever alone!');
 			}
 			
-			$node.addClass('cform-hidden').after($html);
+			base.addToDom($node, $html);
         };
 
         // converts select fields
         base.handleSelect = function($node){
         	var name = $node.attr('name'),
         		value = $node.attr('value'),
+        		multiple = $node.attr('multiple'),
         		$subnodes = $node.find('option'),
         		$selected = $subnodes.filter(':selected'),
         		template = '',
         		$options = $(),
         		$html = $();
 
+			if (typeof attr !== typeof undefined && attr !== false) {
+			    // ...
+			}
 			if (typeof name === typeof undefined && name === false) {
 				name = '';
 			}
-			if (typeof value === typeof undefined && value === false) {
-				value = '';
-			}
-
-			template = $.cForm.defaultOptions.templates['select'];
-			
-			$html = $(template.replace('{{name}}', name)
+			if (typeof multiple === typeof undefined || multiple === false) {
+				multiple = false;
+				template = $.cForm.defaultOptions.templates['select'];
+				$html = $(template.replace('{{name}}', name)
 							.replace('{{text}}', $selected.html()));
+			}else{
+				multiple = true;
+				template = $.cForm.defaultOptions.templates['multiselect'];
+				$html = $(template.replace('{{name}}', name));
+			}
 
 			$subnodes.each(function(index){
 				var $node = $(this),
 					cssclass = $node.prop('selected')?'selected':'',
 					template = $.cForm.defaultOptions.templates['option'];
 
-					template = template.replace('{{value}}', $node.val())
-									.replace('{{text}}', $node.html());
+				template = template.replace('{{value}}', $node.val())
+								.replace('{{text}}', $node.html());
 
 				$options = $options.add($(template).addClass(cssclass));
 			});
@@ -157,24 +163,45 @@
 				{
 					$origin: $node, 
 					$mirror: $html, 
-					$options: $options
+					$options: $options,
+					multiple: multiple
 				},
 				function (event) {
 					var $node = $(this),
-						$origin = event.data.$origin;
-						$mirror = event.data.$mirror;
-						$options = event.data.$options;
+						$origin = event.data.$origin,
+						$mirror = event.data.$mirror,
+						$options = event.data.$options,
+						multiple = event.data.multiple,
 						value = $node.data('value'),
 						text = $node.html();
-	
+						
+					if(!multiple){
 						$options.not($node).removeClass('selected');
 						$node.addClass('selected');
-	
+		
 						$origin.val(value);
-	
+		
 						$mirror.data('value', value)
 							.find('.cform-control')
-								.html(text);
+								.html(text);							
+					}else{
+						var value_array = [];
+						if($node.hasClass('selected')){
+							value_array = $origin.val();
+							value_array.splice(value_array.indexOf(value), 1);
+
+							$origin.val(value_array);
+							$mirror.data('value', value_array);
+							$node.removeClass('selected');
+						}else{
+							value_array = $origin.val()?$origin.val():[];
+							value_array.push(value);
+
+							$origin.val(value_array);
+							$mirror.data('value', value_array);
+							$node.addClass('selected');
+						}
+					}
 				}
 			);
 
@@ -183,31 +210,55 @@
 			$node.bind(
 				'change', 
 				{
-					$mirror: $html, 
-					$options: $options
+					$origin: $node,
+					$mirror: $html,  
+					$options: $options,
+					multiple: multiple
 				}, 
 				function(event){
 					var $node = $(this),
 						value = $node.val(),
 						text = $node.find('option[value="' + value + '"]').html(),
+						$origin = event.data.$origin,
 						$mirror = event.data.$mirror,
-						$options = event.data.$options;
-	
+						$options = event.data.$options,
+						multiple = event.data.multiple;
+
+					if(!multiple){
 						$mirror.data('value', value)
 							.find('.cform-control')
 								.html(text);
-	
+		
 						$options.removeClass('selected')
 							.filter('[data-value="' + value + '"]')
 							.addClass('selected');
-				}
-			);
+					}else{
+						var value_array = $origin.val()?$origin.val():[],
+							i = 0;
+						$mirror.data('value', value_array);
 
+						$options.removeClass('selected');
+						
+						for(i = 0; i < value_array.length; i++){
+							$options.filter('[data-value="' + value_array[i] + '"]')
+								.addClass('selected')
+						};
+					}
+				}
+			)
+			base.addToDom($node, $html);
+        };
+
+        base.addToDom = function($node, $html) {
 			$node.addClass('cform-hidden').after($html);
         };
 
         base.tagName = function($element) {
   			return $element.prop("tagName").toLowerCase();
+		};
+
+		base.createCheckbox = function() {
+
 		};
         
         // Run initializer
@@ -216,19 +267,22 @@
     
     $.cForm.defaultOptions = {
     	templates:		{
-    		text:      	'<div class="cform-text"></div>',
-    		password:   '<div class="cform-text cform-password"></div>',
-    		checkbox:	'<div class="cform-checkbox" data-name="{{name}}">\
-    						<div class="cform-marker"></div>\
-    					</div>',
-    		radio:		'<div class="cform-radio" data-name="{{name}}">\
-    						<div class="cform-marker"></div>\
-    					</div>',
-    		select: 	'<div class="cform-select" data-name="{{name}}">\
-    						<div class="cform-control">{{text}}</div>\
-    						<ul></ul>\
-    					</div>',
-    		option: 	'<li data-value="{{value}}">{{text}}</li>',
+    		text:      		'<div class="cform-text"></div>',
+    		password:   	'<div class="cform-text cform-password"></div>',
+    		checkbox:		'<div class="cform-checkbox" data-name="{{name}}">\
+    							<div class="cform-marker"></div>\
+    						</div>',
+    		radio:			'<div class="cform-radio" data-name="{{name}}">\
+    							<div class="cform-marker"></div>\
+    						</div>',
+    		select: 		'<div class="cform-select" data-name="{{name}}">\
+    							<div class="cform-control">{{text}}</div>\
+    							<ul></ul>\
+    						</div>',
+    		multiselect: '<div class="cform-multiselect" data-name="{{name}}">\
+    							<ul></ul>\
+    						</div>',
+    		option: 		'<li data-value="{{value}}">{{text}}</li>',
     	}
     };
     

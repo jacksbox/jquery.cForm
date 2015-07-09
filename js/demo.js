c3bfd56d05987cf242a9413c0f586dc103f96781
jck7e <mario.j@jacksbox.de>
2015-07-09 13:07:08 +0200

    UPDATE demo page

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){	
 	$('.cform').cForm();
+
+	$('header a').click(function () {
+    	var anchor = $(this).attr('href').slice(1),
+    	 	offset = $('a[name="' + anchor + '"]').offset();
+    	 	$('html, body').animate({
+    			scrollTop: offset.top - 100
+			});
+	});
 });
